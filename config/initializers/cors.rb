d5a5e0ec12c86d10f9062c367286e6177c812183
Nick-Damico <nickalan82@icloud.com>
2018-10-20 09:36:43 -0400

    add second parameter of http://localhost:3000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://foodieq-react.herokuapp.com'
+    origins 'https://foodieq-react.herokuapp.com', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
