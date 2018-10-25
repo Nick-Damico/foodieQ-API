88a9bac4729292e453852d6cad743a7c1c9ae3e6
Nick-Damico <nickalan82@icloud.com>
2018-10-24 22:28:02 -0400

    restrict url requests on 'api/v1/recipes to index actions only

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :recipes
+      resources :recipes, only: [:index]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
