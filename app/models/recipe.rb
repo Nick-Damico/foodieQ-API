79fb09ff506275732eb1e69643cbc49e5324c6df
Nick-Damico <nickalan82@icloud.com>
2018-11-05 13:42:29 -0500

    Add validations for :title, presence and maximum length

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  validates :title, presence: true,
+                    length: { maximum: 100 }
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  let(:recipe) {Recipe.new(title: 'Quick and Easy Pad Thai',
+                      description: 'this is Not a traditional pad Thai
+                      that is like the one served on the side of the road in
+                      Chang mai..nothing is I suppose! But what this is
+                      extremely yummy, and easy to make!'
+                    )}
   describe "Recipe" do
     it "is valid with title, description" do
-      recipe = Recipe.new(title: 'Quick and Easy Pad Thai',
-                          description: 'this is Not a traditional pad Thai
-                          that is like the one served on the side of the road in
-                          Chang mai..nothing is I suppose! But what this is
-                          extremely yummy, and easy to make!'
-                        )
-      expect(recipe).to be_valid        
+      expect(recipe).to be_valid
+    end
+
+    it 'is invalid without title' do
+      recipe.title = ''
+      expect(recipe).to_not be_valid
+    end
+
+    it 'title length cannot be greater than 100characters' do
+      recipe.title = 'A' * 101;
+      expect(recipe).to_not be_valid
     end
   end
 end
