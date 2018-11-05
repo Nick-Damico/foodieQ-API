3de2cc97d26582387f5f6f5fc38cf73b28260edc
Nick-Damico <nickalan82@icloud.com>
2018-11-05 14:37:58 -0500

    Add Validations for 'title', 'description'. Test Passing. Add validation callback #capitalize_description

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,15 @@
 class Recipe < ApplicationRecord
   validates :title, presence: true,
                     length: { maximum: 100 }
+  validates :description, presence: true,
+                          length: { maximum: 1500 }
+
+  before_validation :capitalize_description
+
+  private
+  def capitalize_description
+    if !self.description.empty?
+      self.description = self.description.capitalize
+    end
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -12,14 +12,35 @@ RSpec.describe Recipe, type: :model do
       expect(recipe).to be_valid
     end
 
-    it 'is invalid without title' do
-      recipe.title = ''
-      expect(recipe).to_not be_valid
+    describe 'title' do
+      it 'is invalid without title' do
+        recipe.title = ''
+        expect(recipe).to_not be_valid
+      end
+
+      it 'is invalid with a title length greater then 100characters' do
+        recipe.title = 'A' * 101
+        expect(recipe).to_not be_valid
+      end
     end
 
-    it 'title length cannot be greater than 100characters' do
-      recipe.title = 'A' * 101;
-      expect(recipe).to_not be_valid
+    describe 'description' do
+      it 'is invalid without a description' do
+        recipe.description = ''
+        expect(recipe).to_not be_valid
+      end
+
+      it 'is invalid with a description greater then 1500 characters' do
+        recipe.description = Faker::Lorem.paragraph_by_chars(1501)
+        expect(recipe).to_not be_valid
+      end
+
+      it 'uses a #before_validation callback to captialize first letter' do
+        recipe.description = "best cake yet"
+        recipe.valid?
+        expect(recipe.description).to eq('Best cake yet')
+      end
     end
+
   end
 end
