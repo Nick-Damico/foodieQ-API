e069a1dde38391d406e5e0e94b200df4e44e4a0a
Nick-Damico <nickalan82@icloud.com>
2018-11-06 10:37:19 -0500

    Add test for name validations, Define validations on Ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true,
+                   length: { minimum: 8, maximum: 200 }
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
+  before(:all) do
+      @ingredient = build(:ingredient)
+  end
 
   it "can be created" do
-    ingredient = Ingredient.create!(name: '1 cup flour')
-    expect(ingredient).to be_valid
+    expect(@ingredient).to be_valid
+  end
+
+  it "is invalid with a blank name" do
+    ingredient = build(:ingredient)
+    ingredient.name = ''
+    expect(ingredient).to_not be_valid
+  end
+
+  it 'is valid with name between 8 and 200 characters in length' do
+    expect(@ingredient).to be_valid
+  end
+
+  it 'is invalid with name length less then 8' do
+    ingredient = build(:ingredient)
+    ingredient.name = 'cup tea'
+    expect(ingredient).to_not be_valid
+  end
+
+  it 'is invalid with name lenght greater then 200' do
+    ingredient = build(:ingredient)
+    ingredient.name = Faker::Lorem.sentence(201)
+    expect(ingredient).to_not be_valid
   end
 end
