66fad0fc9f26e13ab1be8198fa9d5e96aad7650c
Nick-Damico <nickalan82@icloud.com>
2018-11-06 13:41:23 -0500

    Add test for invalid attribute values

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Step, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @step = create(:step)
+  end
+
+  it 'can be created' do
+    expect(@step).to be_valid
+  end
+
+  describe 'description' do
+    it 'is invalid with a blank description' do
+      invalid_step = build(:invalid_step)
+
+      expect(invalid_step).to_not be_valid
+      expect(invalid_step.errors[:description]).to include("can't be blank")
+    end
+
+    it "can't have a description longer then 500 characters" do
+      invalid_step = build(:invalid_step_length)
+
+      expect(invalid_step).to_not be_valid
+      expect(invalid.errors[:description]).to include("is too long (maximum length is 500 characters)")
+    end
+  end
 end
