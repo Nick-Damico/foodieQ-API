a290915ef296c54fd08207e93f697bb2a3d94159
Nick-Damico <nickalan82@icloud.com>
2018-11-06 13:40:59 -0500

    Add invalid Factories for Step

--- spec/factories/steps.rb
+++ spec/factories/steps.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :step do
-    description { "MyText" }
+    description { "Add 8 oz of Chicken to broth." }
     recipe_id { 1 }
+
+    factory :invalid_step, parent: :step do
+      description { nil }
+      recipe_id { 1 }
+    end
+
+    factory :invalid_step_length, parent: :step do
+      description { Faker::Lorem.sentence(501)}
+      recipe_id { 1 }
+    end
   end
 end
