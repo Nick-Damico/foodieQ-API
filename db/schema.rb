11cff8f89ffc3c6c827e63bfcd948c6c90c125fb
Nick-Damico <nickalan82@icloud.com>
2018-11-08 14:49:54 -0500

    Rails generate User password_digest, email, admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106180820) do
+ActiveRecord::Schema.define(version: 20181108194414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20181106180820) do
   create_table "steps", force: :cascade do |t|
     t.text "description"
     t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
