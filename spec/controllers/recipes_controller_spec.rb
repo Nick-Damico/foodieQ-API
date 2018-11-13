1b9ee194a1a112e8beec30a0d26e2296421071f9
Nick-Damico <nickalan82@icloud.com>
2018-11-12 20:10:29 -0600

    Test RecipesController request with no Authentication should recieve status of :unauthorized

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+require 'jwt'
 
-RSpec.describe RecipesController, type: :controller do
+RSpec.describe Api::V1::RecipesController, type: :controller do
 
+context 'when the request with NO authentication header' do
+   it 'should return unauth for retrieve current user info before login' do
+     get :index
+     expect(response).to have_http_status(:unauthorized)
+   end
+ end
+
+  it 'sends list of recipes' do
+    # get '/api/v1/recipes'
+    # response = JSON.parse(response.body)
+    # binding.pry
+    # expect(response).to have_http_status(:unauthorized)
+  end
 end
