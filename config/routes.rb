6e658ac4ee5945209571b324b8a9f89b542ee1c7
Nick-Damico <nickalan82@icloud.com>
2018-11-13 21:29:55 -0600

    Add POST /signup /login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
+      post '/signup' => 'users#new'
+      post '/login' => 'sessions#new'
+
       resources :users
-      resources :recipes, only: [:index]
+      resources :recipes
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
