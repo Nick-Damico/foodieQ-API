6275870ff094f8e980b41cd9ed3b04dbaf9ce2fa
Nick-Damico <nickalan82@icloud.com>
2018-11-15 21:10:23 -0600

    Add test to protect data attrbutes of email, password, admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,28 @@ RSpec.describe User, type: :model do
   it 'is valid with a valid email, password, admin attributes' do
     expect(@user).to be_valid
   end
-  it "is invalid without an email"
-  it "is invalid without a last name"
-  it "is invalid without an email address"
+
+  it "is invalid without an email" do
+    @user.email = nil
+
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without a password" do
+    @user.password = nil
+
+    expect(@user).to_not be_valid
+  end
+
+  it "it defaults to an admin value of false" do
+    user = User.new({
+      email: 'valid@example.com',
+      password: 'validUser'
+      })
+
+    expect(user).to be_valid
+    expect(user.admin).to eq(false)
+  end
+
   it "is invalid with a duplicate email address"
 end
