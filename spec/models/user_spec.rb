642ebdd1825ba24836cd862fa33e441f68ce2007
Nick-Damico <nickalan82@icloud.com>
2018-11-15 20:47:27 -0600

    Add model it blocks for test valid and protecting data on Users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = create(:valid_user)
+  end
+
+  it 'is valid with a valid email, password, admin attributes' do
+    expect(@user).to be_valid
+  end
+  it "is invalid without an email"
+  it "is invalid without a last name"
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
 end
