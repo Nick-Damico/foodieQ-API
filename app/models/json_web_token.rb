4c6da35f419e9ea2700b0798f6d29f743b3f2944
Nick-Damico <nickalan82@icloud.com>
2018-12-05 21:59:14 -0500

    Expiration of Token set to 30 minutes, fix error in JWT.decode argument

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,10 +1,10 @@
 class JsonWebToken
   def self.encode(payload)
-    expiration = 60.minutes.from_now.to_i
+    expiration = 30.minutes.from_now.to_i
     JWT.encode payload.merge(exp: expiration), Rails.application.secrets.secret_key_base
   end
 
-  def decode(token)
-    JWT.decode(token, Rails.Rails.application.secrets.secret_key_base).last
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base).first
   end
 end
