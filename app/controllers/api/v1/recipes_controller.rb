d7619f2c76b610d00dfe2a35fa6300859072eec5
Nick-Damico <nickalan82@icloud.com>
2018-12-08 10:00:36 -0500

    Define :create recipe action, with json rendered response for a success and a failure to create resource with error messages, currently authenticate_user! is uncommented out until nested params is tested fully and working

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_recipe, only: [:index, :show]
 
   def index
@@ -13,11 +13,21 @@ class Api::V1::RecipesController < ApplicationController
     end
   end
 
-  private
-  def recipe_params
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      render json: recipe, status: :created
+    else
+      render json: {errors: recipe.errors.full_messages}, status: :bad_request
+    end
   end
 
-  def set_recipe
-    @recipe = Recipe.find_by(id: params[:id])
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :description)
+    end
+
+    def set_recipe
+      @recipe = Recipe.find_by(id: params[:id])
     end
 end
