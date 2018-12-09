979e5890d32a0cce8a52bbcdd54f549990a07799
Nick-Damico <nickalan82@icloud.com>
2018-12-08 21:39:05 -0500

    clean up comments on UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
 
     render json: @users
   end
 
-  # GET /users/1
   def show
     if @user
       render json: @user
@@ -17,7 +15,6 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # POST /users, POST /signup
   def create
     @user = User.new(user_params)
     if @user.save
@@ -27,7 +24,6 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -36,18 +32,15 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find_by(id: params[:id])
     end
-
-    # Only allow a trusted parameter "white list" through.
+    
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
