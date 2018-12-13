f7f0e25cb357280cc719bbfc1dac3e21d302806d
Nick-Damico <nickalan82@icloud.com>
2018-12-12 20:25:18 -0500

    Add tests for Recipes#Controller Api responses

--- spec/requests/recipes_controller_spec.rb
+++ spec/requests/recipes_controller_spec.rb
@@ -3,18 +3,35 @@ require "rails_helper"
 RSpec.describe "RecipesController", :type => :request do
   before do
     DatabaseCleaner.clean
-    @recipe = create(:recipe)
-    @recipe_2 = create(:recipe_2)
+    @user = create(:valid_user)
+    @recipe = build(:recipe)
+    @recipe_2 = build(:recipe_2)
+    @user.recipes.push(@recipe, @recipe_2)
+    @user.save
   end
   describe 'GET /recipes' do
+    # subject { get api_v1_recipes_url }
+    # it { is_expected.to have_http_status(:ok) }
     it 'will retrieve records' do
-      get '/api/v1/recipes'
+      get api_v1_recipes_url
+
       expect(response).to have_http_status(200)
     end
 
     it 'returns a list of recipes' do
-      get '/api/v1/recipes'
-      expect(JSON.parse(response.body).length).to eq(2)
+      get api_v1_recipes_url
+
+      expect(JSON.parse(response.body)["data"].size).to eq(2)
+    end
+
+    it 'will retrieve records' do
+      get '/api/v1/recipes/1'
+
+      expect(response.body).to eq({
+        "data" => {
+          "id" => "1"
+        }
+        })
     end
   end
 end
