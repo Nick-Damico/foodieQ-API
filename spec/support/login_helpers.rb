86abf7485bce1a01054dbb3e64cbabfede349a68
Nick-Damico <nickalan82@icloud.com>
2018-12-15 12:51:22 -0500

    Add #login_user_set_header

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -1,15 +1,18 @@
 module LoginHelpers
   # Login and sign_up methods return a JSON web token, to be captured
   # and used on subsequent requests that require authentication
-
   def login_user(user)
     post api_v1_login_path, :params => {user: {email: user.email,
                                               password: user.password}}
-
     JSON.parse(response.body)
   end
 
   def set_auth_bearer_token(token)
     {"HTTP_AUTHORIZATION" => "Bearer #{token}"}
+  end
+
+  def login_user_set_header(user)
+    token = login_user(user)["token"]
+    set_auth_bearer_token(token)
   end
 end
