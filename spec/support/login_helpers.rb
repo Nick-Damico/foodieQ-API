277922c823a77d9f07c760b3ba78c4f22458fd4d
Nick-Damico <nickalan82@icloud.com>
2018-12-15 12:40:47 -0500

    Add #set_auth_bearer_token

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -6,6 +6,10 @@ module LoginHelpers
     post api_v1_login_path, :params => {user: {email: user.email,
                                               password: user.password}}
 
-    JSON.parse(response.body)    
+    JSON.parse(response.body)
+  end
+
+  def set_auth_bearer_token(token)
+    {"HTTP_AUTHORIZATION" => "Bearer #{token}"}
   end
 end
