7157f3d93cd5320cdbc4469f67563387a81d6445
Nick-Damico <nickalan82@icloud.com>
2018-12-16 18:25:47 -0500

    Add dependent :delete_all on :recipes association to keep orphaned data from being left in db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :recipes
+  has_many :recipes, dependent: :delete_all
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
