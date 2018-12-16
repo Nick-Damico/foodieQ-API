18a70ff1fc541067c2721ca6f5cccbd32df0f5ed
Nick-Damico <nickalan82@icloud.com>
2018-12-16 17:36:43 -0500

    add Tests for PATCH update requests with Authorization, response tested for success and failure with valid and invalid attributes

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -76,5 +76,60 @@ RSpec.describe "UsersController", :type => :request do
     end
   end
 
+  describe 'PATCH :update' do
+    context 'with authorization' do
+      it 'responds successfully with 200' do
+        token = login_user(@user)["token"]
+        patch api_v1_user_path(@user),
+          :headers => set_auth_bearer_token(token),
+          :params => { :user => {email: 'users_one@example.com', password: 'validTest'}}
 
+        expect(response).to have_http_status(200)
+        expect(response).to be_success
+      end
+
+      context 'with valid attributes' do
+        it 'updates attributes' do
+          token = login_user(@user)["token"]
+          patch api_v1_user_path(@user),
+          :headers => set_auth_bearer_token(token),
+          :params => { :user => {email: 'users_one@example.com', password: 'validTest'}}
+
+          attributes = user_attributes
+          expect(attributes["email"]).to eq('users_one@example.com')
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'does not update attributes' do
+            token = login_user(@user)["token"]
+            patch api_v1_user_path(@user),
+            :headers => set_auth_bearer_token(token),
+            :params => { :user => {email: 'invalid.com', password: 'validTest'}}
+            user = User.find(@user.id)
+
+            expect(user.email).to_not eq('invalid.com')
+        end
+      end
+    end
+
+    context 'without authorization' do
+      it 'responds unsuccessfully' do
+        token = login_user(@user)["token"]
+        patch api_v1_user_path(@user),
+        :params => { :user => {email: 'invalid.com', password: 'validTest'}}
+
+        expect(response).to_not be_success
+      end
+
+      it 'does not let a User update another Users attributes' do
+        token = login_user(@user2)["token"]
+        patch api_v1_user_path(@user),
+        :headers => set_auth_bearer_token(token),
+        :params => { :user => {email: 'user123@example.com', password: 'validTest'}}
+
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
 end
