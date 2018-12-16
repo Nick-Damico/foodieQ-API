32aff940eea7abb76ece3026c6f890b04282b866
Nick-Damico <nickalan82@icloud.com>
2018-12-15 20:42:46 -0500

    (3) new tests added for successful fetch of Users :index, :show

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "UsersController", :type => :request do
   before do
     DatabaseCleaner.clean
-    @user = create(:valid_user)
+    @user = create(:user_1)
+    @user2 = create(:user_2)
     @recipe = build(:recipe)
     @recipe_2 = build(:recipe_2)
     @user.recipes.push(@recipe, @recipe_2)
@@ -11,7 +12,13 @@ RSpec.describe "UsersController", :type => :request do
   end
 
   describe 'GET :index' do
-    it 'returns a status of 200 of success' do
+    it 'responds successfully' do
+      get api_v1_users_path
+
+      expect(response).to be_success
+    end
+
+    it 'responds with 200 status' do
       get api_v1_users_path
 
       expect(response).to have_http_status(200)
@@ -22,6 +29,27 @@ RSpec.describe "UsersController", :type => :request do
       json_response = response_to_json(response)
 
       expect(json_response.size).to eq(2)
+    end
+  end
+
+  describe 'GET :show' do
+    it 'responds successfully' do
+      get api_v1_user_path(@user)
+
+      expect(response).to be_success
+    end
+    
+    it 'returns a status of 200' do
+      get api_v1_user_path(@user)
+
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns a users info' do
+      get api_v1_user_path(@user)
+      json_response = response_to_json
+
+      expect(json_response).to
     end
   end
 end
