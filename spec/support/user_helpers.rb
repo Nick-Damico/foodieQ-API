21bb0783ed725fdadde0cccfec226622bcd95516
Nick-Damico <nickalan82@icloud.com>
2018-12-16 16:15:01 -0500

    Add UserHelpers #valid_user_params, #invalid_user_params for supporting requests to :create

--- spec/support/user_helpers.rb
+++ spec/support/user_helpers.rb
@@ -10,6 +10,27 @@ module UserHelpers
   end
 
   def user_attributes
-    response_to_json()["attributes"]    
+    response_to_json()["attributes"]
+  end
+
+  def valid_user_params
+    user_params = {
+      :user => {
+        :email => 'jack@twitter.com',
+        :password  => 'jackTweets',
+        :password_confirmation => 'jackTweets'
+        }
+      }
+  end
+
+  def invalid_user_params
+    # passwords do not match
+    invalid_user_params = {
+      :user => {
+        :email => 'jack@twitter.com',
+        :password  => 'jack',
+        :password_confirmation => 'jackTweets'
+        }
+      }
   end
 end
