4e9934100b7175ac74db22a13b2da24555f3b3c3
Nick-Damico <nickalan82@icloud.com>
2018-12-22 11:00:19 -0500

    Add before_action #recipe_owner to determine if recipe belongs to current_user before modifying. Add before_action #current_user to determine if current_user is the requested user in a nested request. Code refactor clean.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::RecipesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update, :destroy]
-  before_action :set_recipe, only: [:show, :update, :destroy]
-  before_action :correct_user, only: [:update, :destroy]
+  before_action :authenticate_user!, only: %i[create update destroy]
+  before_action :set_recipe, only: %i[show update destroy]
+  before_action :correct_user, only: %i[update destroy]
+  before_action :recipe_owner, only: %i[update destroy]
 
   def index
     @recipes = Recipe.all
-    render json: @recipes, include: ['ingredients', 'steps'], status: :ok
+    render json: @recipes, include: %w[ingredients steps], status: :ok
   end
 
   def show
-    render json: @recipe, include: ['ingredients', 'steps'], status: :ok
+    render json: @recipe, include: %w[ingredients steps], status: :ok
   end
 
   def create
@@ -19,15 +20,15 @@ class Api::V1::RecipesController < ApplicationController
     if recipe.save
       render json: recipe, status: :created
     else
-      render json: {errors: recipe.errors.full_messages}, status: :bad_request
+      render json: { errors: recipe.errors.full_messages }, status: :bad_request
     end
   end
 
   def update
     if @recipe.update_attributes(recipe_params)
-      render json: @recipe, include: ['ingredients', 'steps'], status: :ok
+      render json: @recipe, include: %w[ingredients steps], status: :ok
     else
-      render json: {errors: @recipe.errors.full_messages}, status: :bad_request
+      render json: { errors: @recipe.errors.full_messages }, status: :bad_request
     end
   end
 
@@ -37,44 +38,49 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   private
-    def recipe_params
-      # Nested form submission was resulting in TypeError (no implicit conversion of Symbol into Integer):
-      # Solution is to rewrite the params nested *_attributes to the correct format.
-      if params[:recipe][:ingredients_attributes].present?
-        params[:recipe][:ingredients_attributes] = [{name: params[:recipe][:ingredients_attributes][:name]}]
-      end
-      params.require(:recipe).permit(:id, :title, :description, :user_id,
-        ingredients_attributes: [:id, :name])
+
+  def recipe_params
+    # Nested form submission was resulting in TypeError (no implicit conversion of Symbol into Integer):
+    # Solution is to rewrite the params nested *_attributes to the correct format.
+    if params[:recipe][:ingredients_attributes].present?
+      params[:recipe][:ingredients_attributes] = [{ name: params[:recipe][:ingredients_attributes][:name] }]
     end
+    params.require(:recipe).permit(:id, :title, :description, :user_id,
+                                   ingredients_attributes: %i[id name])
+  end
 
-    def set_recipe
-      @recipe = Recipe.find_by(id: params[:id])
-      if @recipe.nil?
-        render json: {errors: [{
-            status: '404',
-            title: "Record not found",
-            detail: "Record not found with id: #{params[:id]}"
-          }]}, status: :not_found
-      end
+  def set_recipe
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe.nil?
+      render json: { errors: [{
+        status: '404',
+        title: 'Record not found',
+        detail: "Record not found with id: #{params[:id]}"
+      }] }, status: :not_found
     end
+  end
 
-    def build_recipe(params)
-      if params[:user_id].present?
-        user = User.find_by(id: params[:user_id])
-        recipe = user.recipes.build(recipe_params)
-      else
-        recipe = Recipe.new(recipe_params)
-      end
+  def build_recipe(params)
+    if params[:user_id].present?
+      user = User.find_by(id: params[:user_id])
+      recipe = user.recipes.build(recipe_params)
+    else
+      recipe = Recipe.new(recipe_params)
     end
+  end
 
-    # Confirms the current User is the Recipe owner before modify data
-   def correct_user
-     if params[:user_id].present?
-       @user = User.find_by(id: params[:user_id])
+  # Confirms the current User is the Recipe owner before modify data
+  def recipe_owner
+    unless recipe_owner?(current_user, @recipe)
+      render json: { errors: ["User not authorized to modify recipe that doesn't belong to them"] }, status: :bad_request
+    end
+  end
 
-       if !@user.recipes.include?(@recipe)
-         render json: {errors: ["User not authorized to modify recipe that doesn't belong to them"]}, status: :bad_request
-       end
-     end
+  def correct_user
+    if params[:user_id].present?
+      unless current_user?(User.find(params[:user_id]))
+        render json: { errors: ["User not authorized to modify an account that doesn't belong to them"] }, status: :bad_request
+      end
    end
+  end
 end
