f50fe7ba608f916450b7b275f10ff7e91ee577e4
Nick-Damico <nickalan82@icloud.com>
2018-12-25 06:23:31 -0500

    add macro has_one_attached :avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipes, dependent: :delete_all
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
