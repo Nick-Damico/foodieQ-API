958a8c1033f3cdc205f1340b7b4a80687d87387c
Nick-Damico <nickalan82@icloud.com>
2018-12-27 11:07:08 -0500

    Add custom validation for attached :image active storage that validates content_type

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user, optional: true
   has_many :ingredients, dependent: :destroy
   has_many :steps, dependent: :destroy
-
   has_one_attached :image
 
   accepts_nested_attributes_for :ingredients, allow_destroy: true
@@ -12,15 +11,24 @@ class Recipe < ApplicationRecord
                     length: { maximum: 100 }
   validates :description, presence: true,
                           length: { maximum: 1500 }
-
   before_validation :format_title_description
+  validate :correct_image_type
 
   private
-  # Validates, Formats description before presisting to db.
-  def format_title_description
-    if self.description.present? && self.title.present?
-      self.description = self.description.strip.capitalize
-      self.title = self.title.strip.capitalize
+
+    # Validates, Formats description before presisting to db.
+    def format_title_description
+      if description.present? && title.present?
+        self.description = description.strip.capitalize
+        self.title = title.strip.capitalize
+      end
     end
-  end
+
+    def correct_image_type
+      if image.attached?
+        if image.content_type.in?(%w[image/jpg image/png])
+          errors.add(:image, 'image must be of content types: jpg, png')
+        end
+      end
+    end
 end
