7e5f7a79a5bb95e5a10e95581ab1cb5cb7131c8d
Nick-Damico <nickalan82@icloud.com>
2018-12-27 11:44:37 -0500

    Add bang operator to correct_image_type conditonal fixes errors being attached to valid attributes. Add #correct_image_size validation and constant MAX_IMAGE_SIZE for validating image byte size

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,12 +7,14 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :ingredients, allow_destroy: true
   accepts_nested_attributes_for :steps, allow_destroy: true
 
+  MAX_IMAGE_SIZE = 5.megabytes
+  before_validation :format_title_description
   validates :title, presence: true,
                     length: { maximum: 100 }
   validates :description, presence: true,
                           length: { maximum: 1500 }
-  before_validation :format_title_description
   validate :correct_image_type
+  validate :correct_image_size
 
   private
 
@@ -26,8 +28,16 @@ class Recipe < ApplicationRecord
 
     def correct_image_type
       if image.attached?
-        if image.content_type.in?(%w[image/jpg image/png])
-          errors.add(:image, 'image must be of content types: jpg, png')
+        if !image.content_type.in?(%w[image/jpg image/png])
+          errors.add(:image, 'must be of content types: jpg, png')
+        end
+      end
+    end
+
+    def correct_image_size
+      if image.attached?
+        if image.byte_size > MAX_IMAGE_SIZE
+          errors.add(:image, 'is too large, max file size is 5mb')
         end
       end
     end
