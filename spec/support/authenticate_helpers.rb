38bc0ca12e89549e07ce5fa46e66172db6449139
Nick-Damico <nickalan82@icloud.com>
2018-12-28 11:22:24 -0500

    Updated method name #signup to singup_request. Add new #logout_request

--- spec/support/authenticate_helpers.rb
+++ spec/support/authenticate_helpers.rb
@@ -1,9 +1,14 @@
 module AuthenticateHelpers
-  def signup
+  def signup_request
     post api_v1_signup_path,
     :params => { :user => {
       email:                 'user@example.com',
       password:              'test12345',
       password_confirmation: 'test12345'}}
+  end
+
+  def logout_request(token)
+    delete api_v1_logout_path,
+    :headers => set_auth_bearer_token(token)
   end
 end
