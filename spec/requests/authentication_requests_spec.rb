d9d05e8e14fa07d3d47ea7abd1fab4aef677a822
Nick-Damico <nickalan82@icloud.com>
2018-12-28 10:36:46 -0500

    Add before_action DatabaseCleaner.clean method call. Add (2) tests for successfully logging in a User

--- spec/requests/authentication_requests_spec.rb
+++ spec/requests/authentication_requests_spec.rb
@@ -1,21 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "AuthenticationRequests", type: :request do
-  describe "sign_up" do
-    before do
-      DatabaseCleaner.clean
-    end
-    it "creates a new User" do
-      expect {
+RSpec.describe 'AuthenticationRequests', type: :request do
+  before do
+    DatabaseCleaner.clean
+  end
+  describe 'sign up' do
+    it 'creates a new User' do
+      expect do
         signup
-      }.to change(User, :count).by(1)
+      end.to change(User, :count).by(1)
     end
 
-    it "returns a JWT token on succesful sign up" do
+    it 'returns a JWT token on succesful sign up' do
       signup
 
       expect(response.status).to eq(201)
-      expect(parse_response["token"]).to be
+      expect(parse_response['token']).to be
+    end
+  end
+
+  describe 'log in' do
+    before do
+      @user = create(:user_1)
+    end
+
+    it 'successfully logs in a User' do
+      login_user(@user)
+
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns a JWT token' do
+      login_user(@user)
+
+      expect(parse_response['token']).to be
     end
   end
 end
