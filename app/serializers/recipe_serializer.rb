c29681606409d356577ba2e539c352b1ed2fcba3
Nick-Damico <nickalan82@icloud.com>
2018-12-31 16:24:31 -0500

    RecipeSerializer#links method defined for return urls for attached active_storage images. Add include Rails.application.routes.url_helpers to utilize helper method #rails_blob_path

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,14 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :created_at
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :description, :links, :created_at
 
   has_many :ingredients, serializer: IngredientSerializer
   has_many :steps, serializer: StepSerializer
+  has_one  :image
 
+  def links
+    {
+      image_url: rails_blob_path(object.image)
+    }
+  end
 end
