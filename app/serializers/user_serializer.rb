ee4b66ad4c81f491d95252d59f01b39f0f84b1f4
Nick-Damico <nickalan82@icloud.com>
2019-01-01 09:08:23 -0500

    add UserSerializer#links, places links hash with avatar image url if one is attached to the model.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  include Rails.application.routes.url_helpers
+  attributes :id, :email, :links
 
   has_many :recipes
+
+  def links
+    resource_links = {}
+    resource_links["self"] = rails_blob_path(object.avatar) if object.avatar_attachment
+    resource_links
+  end
 end
